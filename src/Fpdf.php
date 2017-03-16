9c081a94f929fc49b43d637e4717c288862d5695
Simon Corless <simon@sico.co.uk>
2017-03-16 09:05:34 +0000

    Initial accessor methods.

--- src/Fpdf.php
+++ src/Fpdf.php
@@ -1,7 +1,33 @@
 <?php
+/**
+ * SiCo FPdf Extension
+ * Simply allows access to some private variables
+ *
+ * @version 16th March 2017
+ * @author Simon Corless simon@sico.co.uk
+ * @copyright Copyright Simon Corless 2017
+ */
 namespace SiCoUK\FpdfExtended;
 
-class Fpdf extends FPDF
+class Fpdf extends \FPDF
 {
+    /**
+     * Threshold used to trigger page breaks
+     * 
+     * @return type
+     */
+    public function getPageBreakTrigger()
+    {
+        return $this->PageBreakTrigger;
+    }
     
+    /**
+     * Current orientation
+     * 
+     * @return type
+     */
+    public function getCurOrientation()
+    {
+        return $this->CurOrientation;
+    }
 }
